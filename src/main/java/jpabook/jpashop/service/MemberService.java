de4530a995a65b041361848e4e6cf2e5fb1f7c8c
jujinyoung <jujinyoung1838@naver.com>
2022-09-18 16:54:19 +0900

    회원 Service 작성 완료

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service    //component이므로 자동으로 빈에 등록
+@Transactional(readOnly = true)
+//@AllArgsConstructor    //멤버필드 생성자 생성
+@RequiredArgsConstructor    //final필드의 생성자 생성
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원가입
+     */
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);    //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+}
